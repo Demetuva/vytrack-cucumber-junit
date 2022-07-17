6c9912f8e44ba2903e0da50292852d037f06a47a
15879 <cakirdemet2005@gmail.com>
2022-07-16 22:32:59 -0600

    cukesRunner is updated

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -7,7 +7,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "html:target/cucumber-report.html",
+        plugin = {"html:target/cucumber-report.html",
+                "json:target/json-reports/cucumber.json",
+                "rerun:target/rerun.txt" ,
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "junit:target/xml-report/cucumber.xml"},
         features = "src/test/resources/features",
         glue="com/vytrack/step_definitions",
         dryRun = false,
